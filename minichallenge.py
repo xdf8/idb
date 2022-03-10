817b256132233e6c3d291f12772f668c8f0c8036
Eric Winter <ericwinter@Erics-MacBook-Air.local>
2022-03-10 11:13:02 +0100

    changed print

--- minichallenge.py
+++ minichallenge.py
@@ -23,9 +23,11 @@ def main():
         humidity = int(round(humidity))
         temperature = int(round(temperature))
         #print("{:02d}:{:02d}:{:02d},{:g},{:g}".format(t.tm_hour, t.tm_min, t.tm_sec, temperature, humidity), flush=True)
-        print('temp: ', temperature)
-        print('hum: ', humidity)
-        print('distance: ', distance)
+        print(
+            'temp: ', temperature,
+            'hum: ', humidity,
+            'distance: ', distance
+            )
 
 
     except KeyboardInterrupt:
