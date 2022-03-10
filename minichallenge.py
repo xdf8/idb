14289532751ac9a62c3bb6d883123e495e2c70f0
Eric Winter <ericwinter@Erics-MacBook-Air.local>
2022-03-10 16:51:01 +0100

    changed print

--- minichallenge.py
+++ minichallenge.py
@@ -2,7 +2,7 @@ import time
 # check, that python lib 'grove' is installed; pip3 freeze | grep grove
 import grove.grove_temperature_humidity_sensor as dht
 import grove.grove_ultrasonic_ranger as usr
-from grove.button.button import Button
+from grove.gpio import GPIO
 
 DHT_PIN = 20 # rasperry pi Pin16, Grove D16
 USR_PIN = 5
@@ -12,11 +12,11 @@ def main():
     # Initialize
     temp_sensor = dht.DHT("11", DHT_PIN)
     ultra_sonic_sensor = usr.GroveUltrasonicRanger(USR_PIN)
-    button = Button(BUTTON_PIN)
+    button = GPIO(BUTTON_PIN, GPIO.IN)
 
     while True:
         try:
-            if button.is_pressed():
+            if button.read():
                 # Try to grab a temp_sensor reading
                 humidity, temperature = temp_sensor.read()
                 distance = ultra_sonic_sensor.get_distance()
@@ -37,7 +37,8 @@ def main():
             
             else:
                 print('Press button to see measurements!', end = '\r')
-                time.sleep(1)
+            
+            time.sleep(0.1)
 
 
 
