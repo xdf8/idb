62c778c4394ca81c97dfecac776f0dd9cb828c62
Eric Winter <ericwinter@Erics-MacBook-Air.local>
2022-03-10 11:54:26 +0100

    changed print

--- button_test.py
+++ button_test.py
@@ -1,12 +1,12 @@
 import time
 import grove.grove_button as button
-
+from grove.gpio import GPIO
 
 BUTTON_PIN = 16
 
 def main():
     # Initialize
-    button = button.Button(BUTTON_PIN)
+    button = GPIO(BUTTON_PIN, GPIO.IN)
 
     while True:
         if button.is_pressed():
