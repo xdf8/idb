2258184ca3eedfc2366b71d63c45878e4649cddd
Eric Winter <ericwinter@Erics-MacBook-Air.local>
2022-03-10 11:26:35 +0100

    changed print

--- minichallenge.py
+++ minichallenge.py
@@ -14,30 +14,34 @@ def main():
     ultra_sonic_sensor = usr.GroveUltrasonicRanger(USR_PIN)
     button = Button(BUTTON_PIN)
 
-    try:
-        while button.is_pressed():
-            # Try to grab a temp_sensor reading
-            humidity, temperature = temp_sensor.read()
-            distance = ultra_sonic_sensor.get_distance()
+    while True:
+        try:
+            if button.is_pressed():
+                # Try to grab a temp_sensor reading
+                humidity, temperature = temp_sensor.read()
+                distance = ultra_sonic_sensor.get_distance()
 
-            # Print the temp_sensor values
-            now = time.time()
-            t = time.localtime(now)
-            humidity = int(round(humidity))
-            temperature = int(round(temperature))
-            distance = int(round(distance))
-            #print("{:02d}:{:02d}:{:02d},{:g},{:g}".format(t.tm_hour, t.tm_min, t.tm_sec, temperature, humidity), flush=True)
-            print(
-                'temp: ', temperature,
-                'hum: ', humidity,
-                'distance: ', distance,
-                end = '\r'
-                )
+                # Print the temp_sensor values
+                now = time.time()
+                t = time.localtime(now)
+                humidity = int(round(humidity))
+                temperature = int(round(temperature))
+                distance = int(round(distance))
+                #print("{:02d}:{:02d}:{:02d},{:g},{:g}".format(t.tm_hour, t.tm_min, t.tm_sec, temperature, humidity), flush=True)
+                print(
+                    'temp: ', temperature,
+                    'hum: ', humidity,
+                    'distance: ', distance,
+                    end = '\r'
+                    )
+            
+            else:
+                print('Press button to see measurements!')
 
 
-    except KeyboardInterrupt:
-        print('\n')
-        pass
+        except KeyboardInterrupt:
+            print('\n')
+            pass
 
 if __name__ == '__main__':
     main()
