4b780b64d0c8103c58b13b1bb180c14084cd23f6
Eric Winter <ericwinter1996@gmail.com>
2023-01-20 11:20:48 +0100

    added api upload

--- minichallenge_pi.py
+++ minichallenge_pi.py
@@ -1,15 +1,18 @@
 import time
+import os
+import csv
+
 # check, that python lib 'grove' is installed; pip3 freeze | grep grove
 import grove.grove_temperature_humidity_sensor as dht
 import grove.grove_ultrasonic_ranger as usr
 from grove.gpio import GPIO
 
-import os
-import csv
+from functions import write_data_to_api
 
-DHT_PIN = 20 # rasperry pi Pin16, Grove D16
+DHT_PIN = 20  # rasperry pi Pin16, Grove D16
 USR_PIN = 5
 BUTTON_PIN = 16
+
 
 def main():
     # Initialize
@@ -29,26 +32,42 @@ def main():
             humidity = int(round(humidity))
             temperature = int(round(temperature))
             distance = int(round(distance))
-            
+
             print(
-                'temp: ', temperature,
-                'hum: ', humidity,
-                'distance: ', distance,
-                end = '\r'
-                )
-            
+                "temp: ",
+                temperature,
+                "hum: ",
+                humidity,
+                "distance: ",
+                distance,
+                end="\r",
+            )
+
             # write to file
-            writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            writer = csv.writer(
+                csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL
+            )
             # add csv header
-            writer.writerow(['timestamp', 'temperature', 'humidity', 'distance'])
-            with open('measurements_pi/data.csv', 'a') as csvfile:
+            writer.writerow(["timestamp", "temperature", "humidity", "distance"])
+            with open("measurements_pi/data.csv", "a") as csvfile:
                 # add data
-                writer.writerow([f'{current_time.tm_hour}:{current_time.tm_min}:{current_time.tm_sec}', temperature, humidity, distance])
+                writer.writerow(
+                    [
+                        f"{current_time.tm_hour}:{current_time.tm_min}:{current_time.tm_sec}",
+                        temperature,
+                        humidity,
+                        distance,
+                    ]
+                )
+            write_data_to_api(
+                temp=temperature, hum=humidity, dist=distance, config_path="config.yaml"
+            )
 
         else:
-            print('Press button to start measuring!', end = '\r')
-        
+            print("Press button to start measuring!", end="\r")
+
         time.sleep(1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
